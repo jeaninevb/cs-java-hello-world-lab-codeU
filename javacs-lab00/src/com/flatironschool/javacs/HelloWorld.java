c6bfc31a9d3dc070fab8e335ace90b4310bee546
Jeanine Burke <jvburke@wustl.edu>
2016-04-15 13:20:14 +0100

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,9 +3,16 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+	String javaVersion = System.getProperty("java.version");
+	int index = javaVersion.indexOf('.');
+	index = javaVersion.indexOf('.', index+1);
+	return Double.parseDouble(javaVersion.substring(0, index));
     }
 
     public static void main(String[] args) {
+
 	    // you can test the output of getVersion() here
-    }
+ 	Double version = getVersion();
+	System.out.println("Java version: " + version);   
+ }
 }
